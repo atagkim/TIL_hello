c014a9240edf0072cfed72b65e1d91bda0f23788
SeongGyun_Park <atagkim@ajou.ac.kr>
2020-06-16 17:20:18 +0900

    탐색한 노드에 대한 재탐색을 막아야함. 메모라이징을 할 수 있느냐 없느냐에 따라 갈림

--- algorithm/bipartiteMatching.cpp
+++ algorithm/bipartiteMatching.cpp
@@ -1,65 +1,100 @@
 #include <stdio.h>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-vector< vector<int> > jobTable;
+vector<vector<int>> jobTable;
 int jobCheckTable[1010];
+int nodeCheckTable[1010];
 int solutionCount;
 
+int jobDFS(int oldOne, int newOne, int jobTarget) {
 
-void dfsMatching(int peopleNum, int totalJob, int currentPeople, int currentJob) {
 
-	if (currentPeople >= peopleNum) {
-		if (solutionCount < currentJob) {
-			solutionCount = currentJob;
-		}
-		return;
-	}
+	nodeCheckTable[oldOne] = 1;
 
-	for (int i = 1; i < jobTable[currentPeople].size(); i++) {
+	for (int i = 0; i < jobTable[oldOne].size(); i++) {
 		
-		if (jobCheckTable[jobTable[currentPeople][i]] == 0) {
-			jobCheckTable[jobTable[currentPeople][i]] = 1;
-			dfsMatching(peopleNum, totalJob, currentPeople + 1, currentJob + 1);
-			jobCheckTable[jobTable[currentPeople][i]] = 0;
+		if (jobCheckTable[jobTable[oldOne][i]] == 0) {
+			jobCheckTable[jobTable[oldOne][i]] = oldOne;
+			jobCheckTable[jobTarget] = newOne;
+			solutionCount++;
+
+			return 1;
+		}
+	}
+
+	for (int i = 0; i < jobTable[oldOne].size(); i++) {
+
+		int dfsTarget = jobCheckTable[jobTable[oldOne][i]];
+		if (nodeCheckTable[dfsTarget] == 0) {
+			if (jobDFS(dfsTarget, oldOne, jobTable[oldOne][i]) == 1) {
+				jobCheckTable[jobTable[oldOne][i]] = newOne;
+				
+				return 1;
+			}
 		}
 
 	}
-
-	dfsMatching(peopleNum, totalJob, currentPeople + 1, currentJob);
+	return -1;
+	
 
 }
 
 
 int main() {
 
-	int n, m, inputBuf, inputBuf2;
-	int jobCheckList[1010] = { 0, };
-
+	int n, m;
 	scanf("%d %d", &n, &m);
+
+	vector<int> deleteZero;
+	jobTable.push_back(deleteZero);
+
+	int eachJobNum, inputBuf;
 	for (int i = 0; i < n; i++) {
 
-		vector<int> jobBuf;
-	
-		scanf("%d", &inputBuf);
-		jobBuf.push_back(inputBuf);
-
-		for (int j = 0; j < inputBuf; j++) {
-			scanf("%d", &inputBuf2);
-			jobBuf.push_back(inputBuf2);
-		}
+		vector<int> person;
 		
-		jobTable.push_back(jobBuf);
+		scanf("%d", &eachJobNum);
+		for (int j = 0; j < eachJobNum; j++) {
+			scanf("%d", &inputBuf);
+
+			person.push_back(inputBuf);
+		}
+		jobTable.push_back(person);
+	}
+
+	
+	int currentJob, nodeFlag = 0;
+	for (int i = 1; i <= n; i++) {
+		
+		for (int j = 0; j < jobTable[i].size(); j++) {
+
+			if (jobCheckTable[jobTable[i][j]] == 0) {
+				jobCheckTable[jobTable[i][j]] = i;
+				solutionCount++;
+				nodeFlag = 1;
+				break;
+			}
+		}
+		if (nodeFlag == 1) {
+			nodeFlag = 0;
+			continue;
+		}
+
+
+		for (int j = 1; j <= n; j++) {
+			nodeCheckTable[j] = 0;
+		}
+
+		for (int j = 0; j < jobTable[i].size(); j++) {
+
+			int dfsTarget = jobCheckTable[jobTable[i][j]];
+			if (nodeCheckTable[dfsTarget] == 0) {
+				if (jobDFS(dfsTarget, i, jobTable[i][j]) == 1) break;
+			}
+		}
 
 	}
 
-	sort(jobTable.begin(), jobTable.end());
-
-
-	dfsMatching(n, m, 0, 0);
-
 	printf("%d", solutionCount);
-
-
 }
