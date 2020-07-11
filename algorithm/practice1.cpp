7a0845c5180d9ad730362b09d0fd50c1b8e12b59
SeongGyun_Park <atagkim@ajou.ac.kr>
2020-07-11 18:11:47 +0900

    단순 브루트로 풀수있는 문제

---
+++ algorithm/practice1.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int hambuck[5];
+int drink[5];
+
+int main() {
+
+	int min = 987654321;
+
+	for (int i = 0; i < 3; i++) {
+		scanf("%d", &hambuck[i]);
+	}
+	for (int i = 0; i < 2; i++) {
+		scanf("%d", &drink[i]);
+		for (int j = 0; j < 3; j++) {
+			int sum = hambuck[j] + drink[i] - 50;
+			if (min > sum) {
+				min = sum;
+			}
+		}
+	}
+
+	printf("%d", min);
+
+}
