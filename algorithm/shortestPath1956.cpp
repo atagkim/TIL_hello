e6359a2996b697be31bb41bc1492d934e8551c60
SeongGyun_Park <atagkim@ajou.ac.kr>
2020-08-21 21:07:52 +0900

    다대다는 플로이드

--- algorithm/shortestPath1956.cpp
+++ algorithm/shortestPath1956.cpp
@@ -3,83 +3,65 @@
 
 using namespace std;
 
-
 struct inputUnit {
 	int end;
 	int cost;
 };
 vector<inputUnit> inputTable[410];
 
-int shortestTable[4][410][410];
+int distTable[410][410];
+
+int inf = 987654321;
 
 void init(int v, int e) {
 
-	for (int i = 1; i <= e; i++) {
-		int start, temp;
-		inputUnit input;
-
-		scanf("%d %d %d", &start, &input.end, &input.cost);
-
-		inputTable[start].push_back(input);
-	}
-
-	for (int j = 1; j <= 2; j++) {
+	for (int j = 1; j <= v; j++) {
 		for (int i = 1; i <= v; i++) {
-			for (int k = 1; k <= v; k++) {
-				shortestTable[j][i][k] = 987654321;
-			}
+			distTable[j][i] = inf;
 		}
 	}
-	for (int i = 1; i <= v; i++) {
-		for (int j = 1; j <= v; j++) {
-			if(i == j)
-				shortestTable[1][i][j] = 0;
-		}
+
+	for (int i = 1; i <= e; i++) {
+		int start, end, cost;
+		
+		scanf("%d %d %d", &start, &end, &cost);
+		
+		distTable[start][end] = cost;
 	}
 
 	return;
 }
 
-void free(int v, int e) {
 
-	for (int i = 1; i <= v; i++) {
-		inputTable[i].clear();
-	}
-	return;
-}
+void floyd(int v, int e) {
 
-void bellman(int v, int e) {
 
-	for (int i = 1; i <= 2 * v - 2; i++) {
-		for (int j = 1; j <= v; j++) {
-			for (int k = 0; k < inputTable[j].size(); k++) {
-				
-				for (int l = 1; l <= v; l++) {
-					if (shortestTable[1][l][j] != 987654321
-						&& shortestTable[1][l][j] + inputTable[j][k].cost < shortestTable[1][l][inputTable[j][k].end]) {
-						shortestTable[1][l][inputTable[j][k].end] = shortestTable[1][l][j] + inputTable[j][k].cost;
-						if (shortestTable[2][l][inputTable[j][k].end] > shortestTable[1][l][j] + inputTable[j][k].cost) {
-							shortestTable[2][l][inputTable[j][k].end] = shortestTable[1][l][j] + inputTable[j][k].cost;
-						}
-					}
-					if (shortestTable[2][l][j] != 987654321
-						&& shortestTable[2][l][j] + inputTable[j][k].cost < shortestTable[2][l][inputTable[j][k].end]) {
-						shortestTable[2][l][inputTable[j][k].end] = shortestTable[2][l][j] + inputTable[j][k].cost;
-					}
-				}
+	for (int k = 1; k <= v; k++) {
+		for (int i = 1; i <= v; i++) {
+			for (int j = 1; j <= v; j++) {
+
+				if (distTable[i][k] != inf && distTable[k][j] != inf
+					&& distTable[i][j] > distTable[i][k] + distTable[k][j])
+
+					distTable[i][j] = distTable[i][k] + distTable[k][j];
 			}
 		}
+	}
+
+	for (int i = 1; i <= v; i++) {
+		for (int j = 1; j <= v; j++) {
+			printf("%d ", distTable[i][j]);
+		}
+		printf("\n");
 	}
 
 	int min = 987654321;
 	for (int i = 1; i <= v; i++) {
-		if (shortestTable[2][i][i] != 987654321 && shortestTable[2][i][i] < min)
-			min = shortestTable[2][i][i];
+		if (min > distTable[i][i]) {
+			min = distTable[i][i];
+		}
 	}
-	if (min != 987654321)
-		printf("%d\n", min);
-	else
-		printf("-1\n");
+	printf("%d", min);
 
 }
 
@@ -91,7 +73,6 @@ int main() {
 
 	init(v, e);
 
-	bellman(v, e);
+	floyd(v, e);
 
-	free(v, e);
 }
