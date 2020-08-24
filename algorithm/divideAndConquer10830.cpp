839e8375622a06589959466581218b9ad250bc60
SeongGyun_Park <atagkim@ajou.ac.kr>
2020-08-24 21:35:14 +0900

    파라미터 미스였음. 오버플로우 걱정되면 비트스캔 위에서 아래로 ㄱㄱ

--- algorithm/divideAndConquer10830.cpp
+++ algorithm/divideAndConquer10830.cpp
@@ -1,3 +1,94 @@
+//
+//#include <stdio.h>
+//
+//struct matrixUnit {
+//	int n;
+//	int a[6][6];
+//};
+//
+//matrixUnit baseMatrix;
+//matrixUnit current;
+//matrixUnit solution;
+//
+//void init(int n, long long b) {
+//
+//	baseMatrix.n = n;
+//	solution.n = n;
+//
+//	for (int i = 1; i <= n; i++) {
+//		for (int j = 1; j <= n; j++) {
+//			scanf("%d", &baseMatrix.a[i][j]);
+//		}
+//		solution.a[i][i] = 1;
+//	}
+//
+//	current = baseMatrix;
+//}
+//
+//void matrixMultiplication(matrixUnit* a, matrixUnit* b) {
+//
+//	int n = a->n;
+//
+//	matrixUnit result;
+//	result.n = n;
+//
+//	for (int i = 1; i <= n; i++) {
+//		for (int j = 1; j <= n; j++) {
+//
+//			result.a[i][j] = 0;
+//
+//			for (int k = 1; k <= n; k++) {
+//				result.a[i][j] += a->a[i][k] * b->a[k][j];
+//				result.a[i][j] %= 1000;
+//			}
+//
+//		}
+//	}
+//
+//	for (int i = 1; i <= n; i++) {
+//		for (int j = 1; j <= n; j++) {
+//			a->a[i][j] = result.a[i][j];
+//		}
+//	}
+//
+//
+//}
+//
+//void calculate(int n, int start, long long end) {
+//
+//	while (end > 0) {
+//
+//		if (end % 2 == 1) {
+//			matrixMultiplication(&solution, &current);
+//		}
+//		
+//		end /= 2;
+//		matrixMultiplication(&current, &current);
+//	
+//	}
+//
+//}
+//
+//
+//int main() {
+//
+//
+//	int n;
+//	long long b;
+//	scanf("%d %lld", &n, &b);
+//
+//	init(n, b);
+//
+//	calculate(n, 1, b);
+//
+//	for (int i = 1; i <= n; i++) {
+//		for (int j = 1; j <= n; j++) {
+//			printf("%d ", solution.a[i][j]);
+//		}
+//		printf("\n");
+//	}
+//
+//}
 
 
 
@@ -38,14 +129,14 @@ void matrixMultiplication(matrixUnit* a, matrixUnit* b) {
 
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= n; j++) {
-
+			
 			result.a[i][j] = 0;
 
 			for (int k = 1; k <= n; k++) {
 				result.a[i][j] += a->a[i][k] * b->a[k][j];
 				result.a[i][j] %= 1000;
 			}
-
+			
 		}
 	}
 
@@ -58,18 +149,34 @@ void matrixMultiplication(matrixUnit* a, matrixUnit* b) {
 
 }
 
-void calculate(int n, int start, long long end) {
+void calculate(int n, long long start, long long end) {
 
-	while (end > 0) {
 
-		if (end % 2 == 1) {
+	while (end > start) {
+
+		if (end < start * 2) {
+			
+			end = end - start;
+			start = 1;
+
 			matrixMultiplication(&solution, &current);
-		}
+			
+			current = baseMatrix;
 		
-		end /= 2;
-		matrixMultiplication(&current, &current);
-	
+		}
+		else {
+
+			start = start * 2;
+
+			matrixMultiplication(&current, &current);
+
+		}
+
 	}
+	
+	matrixMultiplication(&solution, &current);
+
+	return;
 
 }
 
@@ -93,112 +200,3 @@ int main() {
 	}
 
 }
-
-//
-//
-//#include <stdio.h>
-//
-//struct matrixUnit {
-//	int n;
-//	int a[6][6];
-//};
-//
-//matrixUnit baseMatrix;
-//matrixUnit current;
-//matrixUnit solution;
-//
-//void init(int n, long long b) {
-//
-//	baseMatrix.n = n;
-//	solution.n = n;
-//
-//	for (int i = 1; i <= n; i++) {
-//		for (int j = 1; j <= n; j++) {
-//			scanf("%d", &baseMatrix.a[i][j]);
-//		}
-//		solution.a[i][i] = 1;
-//	}
-//
-//	current = baseMatrix;
-//}
-//
-//void matrixMultiplication(matrixUnit* a, matrixUnit* b) {
-//
-//	int n = a->n;
-//
-//	matrixUnit result;
-//	result.n = n;
-//
-//	for (int i = 1; i <= n; i++) {
-//		for (int j = 1; j <= n; j++) {
-//			
-//			result.a[i][j] = 0;
-//
-//			for (int k = 1; k <= n; k++) {
-//				result.a[i][j] += a->a[i][k] * b->a[k][j];
-//				result.a[i][j] %= 1000;
-//			}
-//			
-//		}
-//	}
-//
-//	for (int i = 1; i <= n; i++) {
-//		for (int j = 1; j <= n; j++) {
-//			a->a[i][j] = result.a[i][j];
-//		}
-//	}
-//
-//
-//}
-//
-//void calculate(int n, int start, long long end) {
-//
-//
-//	while (end > start) {
-//
-//		if (end < (long long)start * 2) {
-//			
-//			end = end - start;
-//			start = 1;
-//
-//			matrixMultiplication(&solution, &current);
-//			
-//			current = baseMatrix;
-//		
-//		}
-//		else {
-//
-//			start = start * 2;
-//
-//			matrixMultiplication(&current, &current);
-//
-//		}
-//
-//	}
-//	
-//	matrixMultiplication(&solution, &current);
-//
-//	return;
-//
-//}
-//
-//
-//int main() {
-//
-//
-//	int n;
-//	long long b;
-//	scanf("%d %lld", &n, &b);
-//
-//	init(n, b);
-//
-//	calculate(n, 1, b);
-//
-//	for (int i = 1; i <= n; i++) {
-//		for (int j = 1; j <= n; j++) {
-//			printf("%d ", solution.a[i][j]);
-//		}
-//		printf("\n");
-//	}
-//
-//}
