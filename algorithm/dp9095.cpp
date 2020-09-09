23d90317999c3812b6a39fe0a99f964699cd79dd
SeongGyun_Park <atagkim@ajou.ac.kr>
2020-09-10 06:57:10 +0900

    규칙 안보임

---
+++ algorithm/dp9095.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int dp(int input) {
+
+}
+
+
+int main() {
+	
+	int t;
+	scanf("%d", &t);
+	while (t--) {
+		int input;
+		scanf("%d", &input);
+
+		printf("%d",dp(input));
+
+	}
+}
