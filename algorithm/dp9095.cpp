fb041f37fb4fd24a497cf56ff99d1d8653a0a41b
SeongGyun_Park <atagkim@ajou.ac.kr>
2020-09-25 23:29:46 +0900

    규칙 안보이면 끝

--- algorithm/dp9095.cpp
+++ algorithm/dp9095.cpp
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
-int dp(int input) {
+int dpTable[20];
 
+void dp() {
+
+	dpTable[1] = 1;
+	dpTable[2] = 2;
+	dpTable[3] = 4;
+
+	for (int i = 4; i <= 11; i++) {
+		dpTable[i] = dpTable[i - 1] + dpTable[i - 2] + dpTable[i - 3];
+	}
 }
 
 
@@ -9,11 +18,14 @@ int main() {
 	
 	int t;
 	scanf("%d", &t);
+
+	dp();
+
 	while (t--) {
 		int input;
 		scanf("%d", &input);
 
-		printf("%d",dp(input));
+		printf("%d\n",dpTable[input]);
 
 	}
 }
