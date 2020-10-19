8e92d85a3e5355128543bfd98e5b79b0181c7888
SeongGyun_Park <atagkim@ajou.ac.kr>
2020-10-20 04:00:36 +0900

    init이 n^2으로 터졌음ㅋㅋㅋㅋㅋㅋㅋㅋㅋ

--- algorithm/dfs9466.cpp
+++ algorithm/dfs9466.cpp
@@ -7,75 +7,71 @@ int solution;
 
 void init(int n) {
 
-	for (int i = 1; i <= n; i++) {
-		scanf("%d", &inputTable[i]);
-		visitTable[i] = 0;
-	}
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &inputTable[i]);
+        visitTable[i] = 0;
+        cycleChecker[i] = 0;
+    }
 
-	solution = n;
+    solution = n;
 }
 
 int dfs(int current) {
-	
-	if (visitTable[current] == 1) {
-		return 0;
-	}
-	else if (cycleChecker[current] == 1) {
-		visitTable[current] = 1;
-		solution--;
 
-		return current;
-	}
+    if (visitTable[current] == 1) {
+        return 0;
+    }
+    else if (cycleChecker[current] == 1) {
+        visitTable[current] = 1;
+        cycleChecker[current] = 0;
+        
+        solution--;
+        return current;
+    }
 
-	cycleChecker[current] = 1;
-	int next = inputTable[current];
+    cycleChecker[current] = 1;
+    int next = inputTable[current];
 
-	int revalue = dfs(next);
-	if (revalue != 0) {
-		if (revalue == current) {
-			return 0;
-		}
-		else {
-			visitTable[current] = 1;
-			solution--;
+    int revalue = dfs(next);
+    cycleChecker[current] = 0;
+    
+    if (revalue != 0) {
+        if (revalue == current) {
+            return 0;
+        }
+        else {
+            visitTable[current] = 1;
+            solution--;
 
-			return revalue;
-		}
-	}
-	else {
-		visitTable[current] = 1;
-		return 0;
-	}
+            return revalue;
+        }
+    }
+    else {
+        visitTable[current] = 1;
+        return 0;
+    }
 }
-
-void initChecker(int n) {
-	for (int i = 1; i <= n; i++) {
-		cycleChecker[i] = 0;
-	}
-}
-
 
 int main() {
 
-	int t;
-	scanf("%d", &t);
+    int t;
+    scanf("%d", &t);
 
-	while (t--) {
+    while (t--) {
 
-		int n;
-		scanf("%d", &n);
+        int n;
+        scanf("%d", &n);
 
-		init(n);
-	
-		for (int i = 1; i <= n; i++) {
-			if (visitTable[i] == 0) {
-				initChecker(n);
-				dfs(i);
-			}
-		}
-		
-		printf("%d\n", solution);
-	
-	}
+        init(n);
+
+        for (int i = 1; i <= n; i++) {
+            if (visitTable[i] == 0) {
+                dfs(i);
+            }
+        }
+
+        printf("%d\n", solution);
+
+    }
 
 }
