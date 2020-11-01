ea27239fd70d97cfd42d45cd7ad7a8c41d00fa69
SeongGyun_Park <atagkim@ajou.ac.kr>
2020-11-01 23:25:00 +0900

    ㅋㅋㅋㅋ 경우의수 어케세지 구현력 폭망

---
+++ algorithm/2293dp.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int dpTable[10100];
+int coinTable[110];
+
+int countPossible(int value, int n) {
+
+	for (int i = 1; i < value; i++) {
+
+		int current = dpTable[i];
+
+
+		int idx = n, sum = 0;
+		while (1) {
+			
+			
+
+		}
+
+	}
+
+}
+
+int main() {
+
+	int n, k;
+	scanf("%d %d", &n, &k);
+
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &coinTable[i]);
+	}
+
+	
+	for (int i = 1; i <= k; i++) {
+
+		dpTable[i] = countPossible(i, n);
+	}
+}
