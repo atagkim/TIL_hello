0ed7fb26bdf7e852779d63b748327d86cd2a707c
SeongGyun_Park <atagkim@ajou.ac.kr>
2021-01-04 23:49:31 +0900

    exercise 정리해야함

--- algorithm/exercise/programmersHeap2.cpp
+++ algorithm/exercise/programmersHeap2.cpp
@@ -4,17 +4,7 @@
 
 using namespace std;
 
-struct currentunit {
-
-    bool exist;
-    int id;
-    int start;
-    int remain;
-};
-
-int timetable[510];
-
-struct cmp {
+struct pqcmp {
     bool operator()(vector<int> a, vector<int> b) {
         if (a[1] < b[1])
             return 0;
@@ -23,10 +13,21 @@ struct cmp {
     }
 };
 
-priority_queue<vector<int>, vector<vector<int>>, cmp> pq;
-queue<vector<int>> qqu;
+priority_queue<vector<int>, vector<vector<int>>, pqcmp> possible;
 
-bool compare(vector<int> a, vector<int> b) {
+struct currentunit {
+    bool exist;
+    int id;
+    int start;
+    int remain;
+};
+
+int currenttime = 0;
+int jobsidx = 0;
+
+long long sum;
+
+bool sortcmp(vector<int> a, vector<int> b) {
 
     if (a[0] > b[0])
         return 0;
@@ -34,20 +35,66 @@ bool compare(vector<int> a, vector<int> b) {
         return 1;
 }
 
+void findpossible(vector<vector<int>> jobs) {
+       
+    int jobsnum = jobs.size();
+
+    while (jobsidx < jobsnum) {
+
+        if (jobs[jobsidx][0] <= currenttime) {
+
+            jobs[jobsidx].push_back(jobsidx);
+            possible.push(jobs[jobsidx]);
+
+            jobsidx++;
+        }
+        else
+            break;
+    }
+
+    return;
+}
+
 int solution(vector<vector<int>> jobs) {
     int answer = 0;
-
-    int currenttime = 0;
-    
+ 
+    int jobsnum = jobs.size(), completedjob = 0;
     currentunit current;
     current.exist = 0;
 
-    sort(jobs.begin(), jobs.end(), compare);
+    sort(jobs.begin(), jobs.end(), sortcmp);
 
-    for (int i = 0; i < jobs.size(); i++) {
 
+    while (completedjob < jobsnum) {
+
+        findpossible(jobs);
+
+        if (current.exist == 0 && !possible.empty()) {
+            
+            current.exist = 1;
+            current.id = possible.top()[2];
+            current.start = possible.top()[0];
+            current.remain = possible.top()[1];
+            
+            possible.pop();
+
+        }
+        
+        currenttime++;
+        
+        if (current.exist == 1) {
+            current.remain--;
+
+            if (current.remain <= 0) {
+                sum += currenttime - current.start;
+                current.exist = 0;
+
+                completedjob++;
+            }
+        }
     }
 
+    answer = sum / jobsnum;
 
     return answer;
 }
