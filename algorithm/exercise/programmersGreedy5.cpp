01cfb65d4a654ad06d5fd707142f767419b9d4a2
SeongGyun_Park <atagkim@ajou.ac.kr>
2021-01-09 00:44:27 +0900

    .

--- algorithm/exercise/programmersGreedy5.cpp
+++ algorithm/exercise/programmersGreedy5.cpp
@@ -1,11 +1,85 @@
 #include <string>
-#include <vector>
+#include <queue>
 
 using namespace std;
+
+struct edgeunit {
+    int cost;
+    int start;
+    int end;
+};
+
+struct pqcmp {
+    bool operator() (edgeunit a, edgeunit b) {
+        if (a.cost > b.cost)
+            return 1;
+        else
+            return 0;
+    }
+};
+
+priority_queue<edgeunit, vector<edgeunit>, pqcmp> pq;
+
+bool visittable[110];
+
 
 int solution(int n, vector<vector<int>> costs) {
     int answer = 0;
 
+    // init
+    vector<vector<edgeunit>> edgetable;
+    vector<edgeunit> init;
+    edgeunit input, current;
+
+    int count = 0, sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        edgetable.push_back(init);
+    }
+
+    for (int i = 0; i < costs.size(); i++) {
+        input.cost = costs[i][2];
+        input.end = costs[i][1];
+
+        edgetable[costs[i][0]].push_back(input);
+
+        input.end = costs[i][0];
+
+        edgetable[costs[i][1]].push_back(input);
+    }
+
+    
+    // fire
+    visittable[0] = 1;
+    count++;
+    
+    for (int j = 0; j < edgetable[0].size(); j++) {
+        pq.push(edgetable[0][j]);
+    }
+
+
+    // start
+    while (count < n) {
+
+        current = pq.top();
+        pq.pop();
+
+        if (visittable[current.end] == 1)
+            continue;
+
+        visittable[current.end] = 1;
+        
+        sum += current.cost;
+        count++;
+
+        for (int j = 0; j < edgetable[current.end].size(); j++) {
+            pq.push(edgetable[current.end][j]);
+        }
+
+    }
+    
+
+    answer = sum;
 
     return answer;
 }
---
+++ algorithm/exercise/programmersGreedy6.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> routes) {
+    int answer = 0;
+
+    
+
+    return answer;
+}
+
+int main() {
+
+    vector<vector<int>> routes = {{-20, 15}, {-14, -5}, {-18, -13}, {-5, -3}};
+
+    int answer = solution(routes);
+
+    printf("answer = %d\n", answer);
+}
